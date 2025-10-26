b461bf173c7a996831efcc2db0ad3cd377534ecf
javakishore-veleti <javakishore@gmail.com>
2025-10-25 22:07:27 -0400

    Added Spring-AI-Ollama

---
+++ Spring-AI-Ollama/src/main/java/com/jk/labs/ai/sb/ollama/OllamaAppMain.java
@@ -0,0 +1,11 @@
+package com.jk.labs.ai.sb.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaAppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaAppMain.class,args);
+    }
+}
