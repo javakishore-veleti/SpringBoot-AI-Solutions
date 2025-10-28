8a41e9d9ae68b4dbc73faeb8cdb7e57bece9f867
javakishore-veleti <javakishore@gmail.com>
2025-10-28 19:32:03 -0400

    Added RetrievalAugmentationAdvisor

---
+++ Spring-AI-Apps-Common/src/main/java/com/jk/labs/ai/sb/common/prompts_engg/advisors/AdvisorsConfig.java
@@ -0,0 +1,17 @@
+package com.jk.labs.ai.sb.common.prompts_engg.advisors;
+
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdvisorsConfig {
+
+    @Bean
+    public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
+        return RetrievalAugmentationAdvisor.builder().documentRetriever(
+                VectorStoreDocumentRetriever.builder().vectorStore(vectorStore).topK(5).similarityThreshold(0.5).build()).build();
+    }
+}
