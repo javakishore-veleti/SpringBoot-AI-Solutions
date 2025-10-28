d14455552a80544ca3fea50fe8ba8b970cba4f31
javakishore-veleti <javakishore@gmail.com>
2025-10-27 21:34:49 -0400

    Implemented HrPolicyDocsChatController

--- Spring-AI-Apps-Common/src/main/java/com/jk/labs/ai/sb/common/prompts_engg/rag/vector_dbs/HrPolicyDataLoader.java
+++ Spring-AI-Apps-Common/src/main/java/com/jk/labs/ai/sb/common/prompts_engg/rag/vector_dbs/HrPolicyDataLoader.java
@@ -3,17 +3,21 @@ package com.jk.labs.ai.sb.common.prompts_engg.rag.vector_dbs;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
 public class HrPolicyDataLoader {
 
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
     @Autowired
     private VectorStore vectorStore;
 
@@ -24,6 +28,11 @@ public class HrPolicyDataLoader {
     public void loadPolicyPDF() {
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(policyPdfResource);
         List<Document> docs = tikaDocumentReader.get();
-        vectorStore.add(docs);
+        docs.forEach(doc -> {
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"));
+            doc.getMetadata().put("source", "CorporateHrPolicies-"+timestamp + ".pdf");
+        });
+        TextSplitter textSplitter = TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(500).build();
+        vectorStore.add(textSplitter.split(docs));
     }
 }
